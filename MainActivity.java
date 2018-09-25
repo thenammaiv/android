0f425908fd320b3dfcc455df3ff89156900d855d
thenammaiv <41784141+thenammaiv@users.noreply.github.com>
2018-09-25 17:04:59 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.student.myapplication;
+
+import android.content.Intent;
+import android.media.Rating;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RatingBar;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+CheckBox pizza,coffee,burger;
+    Button buttonOrder;
+ @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
